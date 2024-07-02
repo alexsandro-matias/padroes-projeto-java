29c08b6ef95e518c02a8c76bd4c68689b42afa91
alexsandro-matias <matiasalexsandro@gmail.com>
2024-07-02 04:38:22 -0300

    Retomando os trabalhos

--- designpatterns/questao02/OrdenacaoGenerica.java
+++ designpatterns/questao02/OrdenacaoGenerica.java
@@ -2,32 +2,32 @@ package questao02;
 
 public abstract class OrdenacaoGenerica implements EstrategiaOrdenacao {
 
-	protected int[] vetorDesordenado;
-	protected int[] vetorOrdenado;
+    protected int[] vetorDesordenado;
+    protected int[] vetorOrdenado;
 
-	public void preparaVetores(int[] vetorDesordenado) {
+    public void preparaVetores(int[] vetorDesordenado) {
 
 //		guarda o vetor desordenado e permite que o getSize() funcione.
-		this.vetorDesordenado = vetorDesordenado;
+        this.vetorDesordenado = vetorDesordenado;
 
 //		cria um novo vetor do tamanho certo (correspondente ao vetor desordenado) 
 //		já que o getSize() funciona.
-		vetorOrdenado = new int[getSize()];
+        vetorOrdenado = new int[getSize()];
 
-	}
+    }
 
-	public int getSize() {
-		return vetorDesordenado.length;
-	}
+    public int getSize() {
+        return vetorDesordenado.length;
+    }
 
-	public int[] ordenacaoVetor(int[] vetorDesordenado) {
-		preparaVetores(vetorDesordenado);
-		executaEstrategiaOrdenacao();
+    public int[] ordenacaoVetor(int[] vetorDesordenado) {
+        preparaVetores(vetorDesordenado);
+        executaEstrategiaOrdenacao();
 
-		return this.vetorOrdenado;
+        return this.vetorOrdenado;
 
-	}
+    }
 
-	protected abstract void executaEstrategiaOrdenacao();
+    protected abstract void executaEstrategiaOrdenacao();
 
 }
