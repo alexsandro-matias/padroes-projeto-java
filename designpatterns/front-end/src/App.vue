c36dc036491299cf7fe775a601f5361e49c25063
Bruno Souza <souza.brunoha@gmail.com>
2024-09-02 19:33:45 -0300

    setup do front-end

---
+++ designpatterns/front-end/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-toolbar title="Monte seu PC"/>
+      <v-bottom-navigation>
+        <v-btn>Avançar</v-btn>
+      </v-bottom-navigation>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
