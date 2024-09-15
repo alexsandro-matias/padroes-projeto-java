0d6245393c7d3307b4dc38dbbaad79a6b9ff37fe
Bruno Souza <souza.brunoha@gmail.com>
2024-09-14 23:08:45 -0300

    criacao do builder para montagem do desktop

---
+++ designpatterns/front-end/src/builder/builder.ts
@@ -0,0 +1,12 @@
+import { Desktop } from '@/entities/desktop';
+import { HardwareModel } from '@/entities/hardware-model';
+
+export interface Builder {
+    addCpu(item: HardwareModel): Builder;
+    addMotherboard(item: HardwareModel): Builder;
+    addRam(item: HardwareModel): Builder;
+    addGpu(item: HardwareModel): Builder;
+    addStorage(item: HardwareModel): Builder;
+    addPsu(item: HardwareModel): Builder;
+    addCase(item: HardwareModel): Builder;
+}
---
+++ designpatterns/front-end/src/builder/desktop-builder.ts
@@ -0,0 +1,50 @@
+import { Desktop } from '@/entities/desktop';
+import { HardwareModel } from '@/entities/hardware-model';
+import { Builder } from './builder';
+
+export class DesktopBuilder implements Builder {
+    private desktop: Desktop = new Desktop();
+
+    public reset() {
+        this.desktop = new Desktop();
+    }
+
+    public addCpu(cpu: HardwareModel) {
+        this.desktop.setCpu(cpu);
+        return this;
+    }
+
+    public addMotherboard(motherboard: HardwareModel) {
+        this.desktop.setMotherboard(motherboard);
+        return this;
+    }
+
+    public addRam(ram: HardwareModel) {
+        this.desktop.setRam(ram);
+        return this;
+    }
+
+    public addGpu(gpu: HardwareModel) {
+        this.desktop.setGpu(gpu);
+        return this;
+    }
+
+    public addStorage(storage: HardwareModel) {
+        this.desktop.setStorage(storage);
+        return this;
+    }
+
+    public addPsu(psu: HardwareModel) {
+        this.desktop.setPsu(psu);
+        return this;
+    }
+
+    public addCase(pcCase: HardwareModel) {
+        this.desktop.setCase(pcCase);
+        return this;
+    }
+
+    public getDesktop() {
+        return { ...this.desktop };
+    }
+}
